e213677d6e69bcfd40efcdb34faf80c50ac1bb91
g6714 <venelin.petkov@man.eu>
2018-06-16 08:36:58 +0200

    Added vgg scripts

---
+++ sbin/run_vgg16_eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+JAR_DIR=./build/libs
+CLASS_NAME=com.venelinpetkov.deeplearning.examples.Vgg16TransferEvaluate
+
+# Run the command
+java -cp $JAR_DIR/*.jar $CLASS_NAME \
+     -batchSize 64 \
+     -numEpochs 1 \
+     -trainDir "/data/dogscats/train" \
+     -parameterFilename "vgg16.zip" \
+     -showArchitecture false
